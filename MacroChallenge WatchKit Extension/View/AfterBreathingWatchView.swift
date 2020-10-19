d80c6862d9e4a0a27a84501a04610df2a2ab41c0
Kenji Surya Utama <kenjisuryautama@gmail.com>
2020-10-19 10:11:09 +0700

    add simple navigation for watch + helpers folder

---
+++ MacroChallenge WatchKit Extension/View/AfterBreathingWatchView.swift
@@ -0,0 +1,20 @@
+//
+//  AfterBreathingWatchView.swift
+//  MacroChallenge WatchKit Extension
+//
+//  Created by Kenji Surya Utama on 19/10/20.
+//
+
+import SwiftUI
+
+struct AfterBreathingWatchView: View {
+    var body: some View {
+        Text("Ini After Breathing")
+    }
+}
+
+struct AfterBreathingWatchView_Previews: PreviewProvider {
+    static var previews: some View {
+        AfterBreathingWatchView()
+    }
+}
