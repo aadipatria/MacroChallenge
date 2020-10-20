268afeec8f2c506271f703f0b42b5f445ea6f7f5
yudisthirogithub <yudis_deon@yahoo.com>
2020-10-20 10:16:00 +0700

    Simple list  + function call

--- MacroChallenge/View/Emergency Page/EmergencyView.swift
+++ MacroChallenge/View/Emergency Page/EmergencyView.swift
@@ -1,34 +1,73 @@
 //
-//  EmergencyView.swift
-//  MacroChallenge
+//  ContentView.swift
+//  testingCallFile
 //
-//  Created by Kenji Surya Utama on 13/10/20.
+//  Created by Yudis Huang on 05/10/20.
+//  Copyright © 2020 Yudis Huang. All rights reserved.
 //
 
 import SwiftUI
 
-struct EmergencyView: View {
+struct contactDetail : Identifiable {
+    var id : Int = 0
+    var name : String = ""
+    var phoneNumber : String = ""
+    var photo : String = ""
+    
+}
+
+struct ContentView: View {
+    
+//    var contacts = [contactDetail]()
+    
+    var contacts : [contactDetail] = [
+        .init(id: 0, name: "Yudis", phoneNumber: "0895378412968", photo: "Lmfao"),
+        .init(id: 1, name: "Henny", phoneNumber: "0928828228282", photo: "GGWP")
+    ]
+    
+    
     var body: some View {
-        Button(action: {
-            call(number: "123456")
-        }) {
-            Text("Emergency Call")
+       
+        NavigationView {
+        List{
+            ForEach(contacts){
+                i in
+                VStack{
+                    
+                    HStack{
+                        HStack{
+                            Text("Photo")// photo
+                            VStack{
+                                Text("Name")
+                                Text("Phone Number")
+                                }
+                        }
+                    Spacer()
+                        Button(action: {self.call()
+                            
+                        }){
+                            Text("Call")}
+                    }
+                }.foregroundColor(.gray)
+            }
+            
+        }.navigationBarTitle(Text("My Contacts"))
         }
     }
     
-}
-extension EmergencyView{
-    func call(number : String){
-        // this is where the variable emergencyPhoneNumber assigned a value of coredata
-        guard let phoneNumber =  number as String?, let url = URL(string:"telprompt://\(phoneNumber)") else {
+    func call(){
+        var emergencyPhoneNumber = "123456"
+        
+        guard var phoneNumber =  emergencyPhoneNumber as String?, let url = URL(string:"telprompt://\(phoneNumber)") else {
             return
         }
+        
         UIApplication.shared.open(url)
     }
 }
 
-struct EmergencyView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmergencyView()
+        ContentView()
     }
 }
