c2a90729ad0e8f0108c898301e38a3ae22ec008c
Kenji Surya Utama <kenjisuryautama@gmail.com>
2020-11-02 12:58:49 +0700

    basic interaction on emergency view

--- MacroChallenge/Helpers/NavigationPop.swift
+++ MacroChallenge/Helpers/NavigationPop.swift
@@ -11,4 +11,5 @@ final class NavigationPopObject: ObservableObject {
     @Published var toHome: Bool = false
     @Published var toBreathing : Bool = false
     @Published var page : Int = 1
+    @Published var emergency : Bool = false
 }
