839556b209079f3bf6992870e7553bc76165fbe7
Kenji Surya Utama <kenjisuryautama@gmail.com>
2020-11-12 23:37:27 +0700

    kill haptic, change "get started" font

--- MacroChallenge/View/Breath Page/BreathView.swift
+++ MacroChallenge/View/Breath Page/BreathView.swift
@@ -77,9 +77,9 @@ struct BreathView: View {
                         }
                         Spacer()
                         Button(action: {
+                            cancelHaptic()
                             isBreathing = false
                             self.success = false
-                            cancelHaptic()
                             navPop.toBreathing = true
                         }, label: {
                             Text("Stop")
@@ -184,7 +184,6 @@ struct BreathView: View {
                         if isBreathing{
                             showStop = false
                             self.success = true
-                            prepareHaptics()
                             getNumberOfCycles()
                         }else{
                             self.success = false
@@ -204,6 +203,7 @@ struct BreathView: View {
             }
             .frame(maxHeight: ScreenSize.windowHeight() * 0.52)
             .onAppear(perform: {
+                prepareHaptics()
                 success = true
                 showStop = false
                 self.cycleTime = 1
