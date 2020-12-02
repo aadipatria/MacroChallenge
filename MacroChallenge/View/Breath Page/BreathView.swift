dbe22d70b466f54fff131fdecb65c89548c50c53
Kenji Surya Utama <kenjisuryautama@gmail.com>
2020-12-02 23:46:20 +0700

    fix height

--- MacroChallenge/View/Breath Page/BreathView.swift
+++ MacroChallenge/View/Breath Page/BreathView.swift
@@ -105,7 +105,7 @@ struct BreathView: View {
                                         Blur(style: .regular)
                                             .mask(RoundedRectangle(cornerRadius: 15))
                                     )
-                                    .frame(width: 283, height: 327, alignment: .center)
+                                    .frame(width: 283, height: ScreenSize.windowHeight() * 0.4, alignment: .center)
                                 VStack (spacing : 8){
                                     Text(breaths[i].name!)
                                         .font(Font.custom("Poppins-Bold", size: 28, relativeTo: .body))
@@ -166,6 +166,7 @@ struct BreathView: View {
                         }
                     }
                     .modifier(ScrollingHStackModifier(items: breaths.count, itemWidth: 283, itemSpacing: 30))
+                    .frame(width: 283, height: ScreenSize.windowHeight() * 0.4, alignment: .center)
                     .padding(.bottom)
                     
                     ZStack(alignment: .leading) {
